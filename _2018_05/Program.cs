4093bb474ec22577e6953e8ace4e8286601ad58c
Joshua Hegedus <jhegedus9@gmail.com>
2022-04-12 15:58:14 +0200

    Add project files.

---
+++ _2018_05/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2018_05
+{
+    internal class Program
+    {
+        public const string utvonal = "../../../ajto.txt";
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
