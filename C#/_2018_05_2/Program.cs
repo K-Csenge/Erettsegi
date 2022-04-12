f17bb2fc385a02ae0edf09432a6b73cdec2c4443
Joshua Hegedus <jhegedus9@gmail.com>
2022-04-12 20:58:15 +0200

    Feladatok

--- C#/_2018_05_2/Program.cs
+++ C#/_2018_05_2/Program.cs
@@ -24,6 +24,7 @@ namespace _2018_05_2
                 this.irany = irany;
                 athaladas = 0;
             }
+
         }
         static void Main(string[] args)
         {
@@ -48,19 +49,46 @@ namespace _2018_05_2
 
             #region 2. Feladat
 
-            var legutolso = belepesek .Where(
-                j => 
-                j.ora == belepesek.Max(elem => elem.ora) && 
-                j.perc == belepesek.Max(elem => elem.perc)
-                ).LastOrDefault();
+            //var legutolso = belepesek .Where(
+            //    j => 
+            //    j.ora == belepesek.Max(elem => elem.ora) && 
+            //    j.perc == belepesek.Max(elem => elem.perc)
+            //    ).LastOrDefault();
 
-            var legelso = belepesek.Where(
-                j =>
-                j.ora == belepesek.Min(elem => elem.ora) &&
-                j.perc == belepesek.Min(elem => elem.perc)
-                ).FirstOrDefault();
+            //var legelso = belepesek.Where(
+            //    j =>
+            //    j.ora == belepesek.Min(elem => elem.ora) &&
+            //    j.perc == belepesek.Min(elem => elem.perc)
+            //    ).FirstOrDefault();
 
+            var legelso = new Belepes();
+            legelso.ora = 15;
+            legelso.perc = 60;
 
+            var legutolso = new Belepes();
+            legutolso.ora = 0;
+            legutolso.perc = 0;
+
+            for (int i = 0; i < belepesek.Count; i++)
+            {
+                if(belepesek[i].ora <= legelso.ora)
+                {
+                    if(belepesek [i].perc <= legelso.perc)
+                    {
+                        legelso = belepesek [i];
+                    }
+                }
+
+                if (belepesek[i].ora >= legutolso.ora)
+                {
+                    if (belepesek[i].perc >= legutolso.perc)
+                    {
+                        legutolso = belepesek[i];
+                    }
+                }
+            }
+
+            var test = legelso;
             //Console.WriteLine($"Eloszor belepett ember id-je: {belepesek[0].id}, ora: {belepesek[0].ora}");
             //Console.WriteLine($"Utoljara belepett ember id-je: {belepesek[^3].id}, ora: {belepesek[^1].ora}");
             //Console.WriteLine();
