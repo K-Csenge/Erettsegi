07b1868e2ec29cdb0e2bb8d04349c17447da4db4
Joshua Hegedus <jhegedus9@gmail.com>
2022-04-23 21:01:34 +0200

    Ujabb valtozasok

--- C#/_2018_05_2/Program.cs
+++ C#/_2018_05_2/Program.cs
@@ -12,6 +12,7 @@ namespace _2018_05_2
         {
             public int ora;
             public int perc;
+            public int ido;
             public int id;
             public string irany;
             public int athaladas;
@@ -23,9 +24,11 @@ namespace _2018_05_2
                 this.id = id;
                 this.irany = irany;
                 athaladas = 0;
+                ido = ora * 60 + perc;
             }
 
         }
+
         static void Main(string[] args)
         {
             #region 1. Feladat
@@ -49,55 +52,103 @@ namespace _2018_05_2
 
             #region 2. Feladat
 
-            var legutolso = belepesek.Where(
-                j =>
-                j.ora == belepesek.Max(elem => elem.ora) &&
-                j.perc == belepesek.Max(elem => elem.perc) &&
-                j.irany == "ki").LastOrDefault();
+            Belepes legelso;
+            Belepes legutolso;
+            legelso.id = 0;
+            legutolso.id = 0;
+            legelso.ido = 15 * 60 + 60;
+            legutolso.ido = 0;
 
-            var ora = belepesek.Where(elem => 
-                elem.ora == belepesek.Min(elem => elem.ora));
-            
-            var perc = belepesek.Min(elem => elem.perc);
+            for (int i = belepesek.Count - 1; i >= 0; i--)
+            {
+                if (belepesek[i].ido <= legelso.ido && belepesek[i].irany == "be")
+                {
+                    legelso = belepesek[i];
+                }
+            }
 
-            var legelso = belepesek.Where(
-                j =>
-                j.ora == ora &&
-                j.perc ==  perc &&
-                j.irany == "be");
+            for (int i = 0; i <= belepesek.Count - 1; i++)
+            {
+                if (belepesek[i].ido >= legutolso.ido && belepesek[i].irany == "ki")
+                {
+                    legutolso = belepesek[i];
+                }
+            }
 
-            //var legelso = new Belepes();
-            //legelso.ora = 15;
-            //legelso.perc = 60;
-
-            //var legutolso = new Belepes();
-            //legutolso.ora = 0;
-            //legutolso.perc = 0;
-
-            //for (int i = belepesek.Count -1; i >= 0 ; i--)
-            //{
-            //    if(belepesek[i].ora <= legelso.ora)
-            //    {
-            //        if(belepesek[i].perc <= legelso.perc)
-            //        {
-            //            if(belepesek[i].irany == "be")
-            //            {
-            //                legelso = belepesek[i];
-            //            }
-            //        }
-            //    }
-
-
-            //}
-
-            var test = legelso;
-            //Console.WriteLine($"Eloszor belepett ember id-je: {belepesek[0].id}, ora: {belepesek[0].ora}");
-            //Console.WriteLine($"Utoljara belepett ember id-je: {belepesek[^3].id}, ora: {belepesek[^1].ora}");
-            //Console.WriteLine();
+            Console.WriteLine($"Eloszor belepett ember id-je: {legelso.id}");
+            Console.WriteLine($"Utoljara belepett ember id-je: {legutolso.id}");
+            Console.WriteLine();
 
             #endregion
 
             #region 3. Feladat
+
+            int[] athaladasok = new int[101];
+            for(var i = 0; i < 101; i++)
+            {
+                athaladasok[i] = 0;
+            }
+
+            for(int i = 0; i <= belepesek.Count - 1; i++)
+            {
+                athaladasok[belepesek[i].id]++;
+            }
+            
+            var athaladasokString = new List<string>();
+
+            for(int i = 0; i < 101; i++)
+            {
+                if(athaladasok[i] > 0)
+                {
+                    athaladasokString.Add($"{i} {athaladasok[i]}");
+                }
+            }
+
+            var sorok123 = File.ReadLines(utvonal);
+            File.WriteAllLines("../../../athaladasok.txt", athaladasokString);
+            
+            #endregion
+
+            #region 4. Feladat
+            var bentmaradottak = new List<Belepes>();
+
+            //foreach (var belepes in belepesek)
+            //{
+            //    if (belepes.irany == "be")
+            //    {
+            //        bentmaradottak.Add(belepes);
+            //    }
+            //    else
+            //    {
+            //        if (bentmaradottak.Contains(belepes))
+            //        {
+            //            bentmaradottak.Remove(belepes);
+            //        }
+            //    }
+            //}
+
+            //int count = 0;
+            //for (int i = 0; i < belepesek.Count; i++)
+            //{
+            //    count = 0;
+            //    for (int j = 0; j < belepesek.Count; j++)
+            //    {
+            //        if (belepesek[i].id == belepesek[j].id)
+            //        {
+            //            //Console.WriteLine($"I: {i}, J: {j}, Egyformak, id: {belepesek[i].id}");
+            //            count++;
+            //        }
+            //    }
+            //    if (count % 2 != 0)
+            //    {
+            //        bentmaradottak.Add(belepesek[i]);
+            //        Console.WriteLine($"Id: {belepesek[i].id}, Megjeleness: {count}");
+            //    }
+            //}
+
+            //var rendezettBentmaradottak = bentmaradottak.OrderBy(jarkalo => jarkalo.id);
+
+
             #endregion
         }
     }
